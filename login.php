d60183cee761d2815d6c0fdfb1eb87fa7f63338d
vineeth <vineeth532x@gmail.com>
2022-04-14 17:36:28 +0530

    [Vineeth] Initial commit

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
