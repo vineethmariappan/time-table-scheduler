73930d4d646591ca6f64f859a84b182afdfc15e6
vineeth <vineeth532x@gmail.com>
2022-04-14 23:41:02 +0530

    [Vineeth] Add. Timetable cell options

---
+++ tablecelloption.php
@@ -0,0 +1,11 @@
+<div class="form-group">
+    <select  aria-label="Default select example">
+        <option selected>Sub-code</option>
+        <option value="">Sample code</option>
+    </select>
+    <select  aria-label="Default select example">
+        <option selected>Faculty name</option>
+        <option value="">Sample name</option>
+    </select>
+
+</div>
